d08e0838af23f820aa143990fb28ecb32b9994e4
Kate <katekoshurba@gmail.com>
2019-02-21 20:30:09 +0200

    test

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$("#navToggle").click(function() {
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("open");
+    // this line ▼ prevents content scroll-behind
+    $("body").toggleClass("locked");
+});
+$('.overlay').click(function() {
+    $(this).removeClass('open');
+    $('.navBurger').removeClass('active');
+});
+
+
+$('.slider').slick();
+
